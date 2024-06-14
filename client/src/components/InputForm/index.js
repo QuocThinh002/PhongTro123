db26f66a17f02a7a9eff611ba3b21317bd424e5e
quocthinh002 <thinhnq.dev@gmail.com>
2024-06-14 16:47:26 +0700

    register and token

--- client/src/components/InputForm/index.js
+++ client/src/components/InputForm/index.js
@@ -1,11 +1,17 @@
 import { memo } from "react";
 
 const InputForm = (props) => {
-    const {label, id, type} = props
+    const {label, id, type, value, setValue, typeSetValue} = props
     return (<>
         <div>
             <label htmlFor={id} className="text-sm">{label}</label>
-            <input type={type} id={id}  className="w-full text-xl font-bold bg-[#e8f0fe] py-2 px-3 rounded-md outline-none"/>
+            <input
+                type={type}
+                id={id}
+                value={value}
+                onChange={(e) => setValue(prev => ({...prev, [typeSetValue]: e.target.value}))}
+                className="w-full text-xl font-bold bg-[#e8f0fe] py-2 px-3 rounded-md outline-none"
+            />
         </div>
     </>)
 }
--- client/src/pages/Public/Login/index.js
+++ client/src/pages/Public/Login/index.js
@@ -1,27 +1,36 @@
 import { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
+import {useDispatch} from 'react-redux'
+
+import * as actions from '../../../store/actions'
 
 import Button from '../../../components/Button';
 import InputForm from '../../../components/InputForm';
 
 const Login = () => {
+    const dispatch = useDispatch()
     const location = useLocation();
     const flag = (location.state?.flag != null) ? location.state?.flag : true;
     const [isLogin, setIsLogin] = useState(flag);
+    const [payload, setPayload] = useState({phone: '', password: '', fullName: ''})
 
     useEffect(() => {
         setIsLogin(flag);
     }, [flag])
 
+    const handleSubmit = async () => {
+        dispatch(actions.register(payload))
+    }
+
     return (<>
         <div className='bg-white w-[600px] p-[30px] pb-[100px] rounded-md'>
             <h3 className='font-bold text-2xl mb-2'>{isLogin ? 'Đăng nhập':'Tạo tài khoản mới' }</h3>
             <div className='w-full flex flex-col gap-4 mb-5'>
-                {!isLogin && <InputForm type='text' label='HỌ TÊN' id='fullname' />}
-                <InputForm type='text' label='SỐ ĐIỆN THOẠI' id='phone' />
-                <InputForm type='password' label='MẬT KHẨU' id='password' />
+                {!isLogin && <InputForm type='text' label='HỌ TÊN' id='fullname' value={payload.fullName} setValue={setPayload} typeSetValue='fullName' />}
+                <InputForm type='text' label='SỐ ĐIỆN THOẠI' id='phone' value={payload.phone} setValue={setPayload} typeSetValue='phone' />
+                <InputForm type='password' label='MẬT KHẨU' id='password' value={payload.password} setValue={setPayload} typeSetValue='password' />
             </div>
-            <Button dataButton={{ text:  (isLogin ?  'Đăng nhập':'Tạo tài khoản'), bgColor: 'bg-secondary1', textColor: 'text-white', width: 'w-full', hoverColor: 'hover:bg-blue-700' }} />
+            <Button dataButton={{ onClick: handleSubmit, text:  (isLogin ?  'Đăng nhập':'Tạo tài khoản'), bgColor: 'bg-secondary1', textColor: 'text-white', width: 'w-full', hoverColor: 'hover:bg-blue-700' }} />
             <div className='mt-3 flex items-center justify-between'>
                 {isLogin ? <>
                     <span className='text-[blue] hover:text-[orange] cursor-pointer'>Bạn quên mật khẩu?</span>
